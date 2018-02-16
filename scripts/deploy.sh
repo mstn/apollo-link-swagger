7d6de28ea86373be5d709359de46db2849331a6a
mstn <mstn@users.noreply.github.com>
2018-02-16 05:54:52 +0100

    + deploy scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# cleanup lib directory
+rm -rf lib || exit 0;
+mkdir lib;
+
+# compile to javascript
+npm run compile
+
+# build vanilla package.json
+node -e "var package = require('./package.json'); \
+  delete package.scripts; \
+  delete package.options; \
+  delete package.jest; \
+  delete package.devDependencies; \
+  package.main = 'index.js'; \
+  package.typings = 'index.d.ts'; \
+  var fs = require('fs'); \
+  fs.writeFileSync('./lib/package.json', JSON.stringify(package, null, 2)); \
+  "
+
+# Copy to ./npm
+cp README.md lib/
+cp LICENSE lib/
+
+# publish to npm
+cd npm && npm publish --access public

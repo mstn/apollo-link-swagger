ca51227a159d534f7bf775037d2fdc522ceec3d9
mstn <mstn@users.noreply.github.com>
2018-02-16 05:56:01 +0100

    fix type export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import * as Swagger from 'swagger-schema-official';
 
 export type SwaggerSchema = Swagger.Spec;
+
+export { default as loadSwaggerSchema } from './loadSwaggerSchema';
+export * from './swaggerLink';
